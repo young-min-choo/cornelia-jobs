bcca01a7113b455e345457b874777f2191080019
YoungMinChoo <youngminchoo001@gmail.com>
2024-10-29 07:47:31 +0900

    Add job creation

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,28 @@
         <button @click="handleClick('postTime')">Sort by Posted Time</button>
       </div>
     </header>
+    <div>
+      <button @click="createFlag = !createFlag">Create Job</button>
+    </div>
+    <div v-if="createFlag" class="job-input">
+      <div class="input-items-wrapper">
+        <label for="create-title"> Title </label>
+        <input type="text" id="create-title" :value="newJob.title">
+
+        <label for="create-title"> Location </label>
+        <input type="text" id="create-location" :value="newJob.location">
+  
+        <label for="create-title"> Salary </label>
+        <input type="number" id="create-salary" :value="newJob.salary">
+      </div>
+      <button @click="createJob(newJob)">Submit</button>
+    </div>
     <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from 'vue';
+import { defineComponent, ref} from 'vue';
 import JobList from './components/JobList.vue'
 import Job from './types/Job';
 import OrderTerm from './types/OrderTerm';
@@ -27,7 +43,15 @@ export default defineComponent({
     JobList
   },
   setup() {    
+    let createFlag = ref(false);
     const baseURL = 'http://localhost:3000'
+    const newJob: Job = {
+      id: uuidv4(),
+      title: '',
+      location: '',
+      salary: 0,
+      postTime: new Date()
+    }
     const jobs = ref<Job[]>([
       {
         title: 'Help The Priest',
@@ -58,25 +82,6 @@ export default defineComponent({
         postTime: new Date(2024,10,8)
       },
     ])
-    // console.log(jobs.value.push())
-    // fetch(baseURL + '/api/jobs', {
-    //   method: 'GET',
-    //   headers: {
-    //     "Content-type": "application/json",
-    //   },
-
-    // }).then((response) => {
-    //   response.json()
-      
-    //   .then(res => {
-    //     // console.log(jobs.value)
-    //     console.log(res.jobs)
-    //     jobs.value.concat(res.jobs)
-    //     console.log(jobs.value)
-    //   })
-    // }).catch(err => {
-    //   console.error(err)
-    // })
     async function fetchJobs(): Promise<Job[]> {
       try {
         const response = await fetch(baseURL + '/api/jobs', {
@@ -99,6 +104,29 @@ export default defineComponent({
         throw error;  // Rethrow the error so it can be handled by the caller
       }
     }
+    async function createJob(job: Job) {
+      try {
+        // job.id = uuidv4()
+        console.log("Job: ", job)
+        const response = await fetch(baseURL + '/api/jobs/create', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(job),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data)
+        jobs.value.push(data)
+      } catch (error) {
+        console.error("Error creating job:", error);
+        throw error
+      }
+    }
     fetchJobs()
       .then(jobsRetrieved => {
         console.log("Jobs: ", jobsRetrieved);
@@ -118,7 +146,11 @@ export default defineComponent({
       order.value = term
     }
 
-    return {jobs, handleClick, order, baseURL}
+    // const create = () => {
+    //   createFlag = !createFlag;
+    //   console.log(createFlag.valueOf())
+    // }
+    return {jobs, handleClick, order, baseURL, createJob, newJob, createFlag}
   },
   methods: {
     // changeName(name: string) {
@@ -147,5 +179,12 @@ export default defineComponent({
     border-radius: 4px;
     cursor: pointer;
     font-weight: bold;
+  }
+  .job-input {
+    display: flex;
+    justify-content: center;
+  }
+  .input-items-wrapper {
+    display: grid;
   }
 </style>
